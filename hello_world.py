e3209623d511ee0ea61ba605a5a2ac49bc91c103
Tiffany Ricks <tiffany@hacware.com>
2021-05-07 14:25:36 -0400

    test

--- hello_world.py
+++ hello_world.py
@@ -7,8 +7,9 @@ path = '/api/v1/auth/'
 base_url = 'https://%s' % (domain);
 route = base_url + path
 
+data_url = "https://username:password@example.com"
 
-data = { 'uid':'a537bedb0ddacdee597bc7c762', 'sec':'D7KY1SJ4R6XVGFI6R9WGDTA0FHE263M92NSSYXWNCU1WCYP3XCWQCDFNQVNFGO' }
+data = { 'username':'a537bedb0ddacdee597bc7c762', 'password':'D7KY1SJ4R6XVGFI6R9WGDTA0FHE263M92NSSYXWNCU1WCYP3XCWQCDFNQVNFGO' }
 
 r = requests.post(route, data = data)
 connection_info = r.json()
